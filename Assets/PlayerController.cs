ab1bc5f9f1666e3f9cd52c96346beec79de81e9f
GoodbyeBlues <elizabeth.graham2000@gmail.com>
2021-06-05 10:19:57 -0500

    Initial Commit
    The game has the basic flapping mechanic, but no game mechanics like the pillars or moving forward.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float flapForce = 5.0f;
+    public KeyCode flapButton = KeyCode.Space;
+    public float maxHeight = 4.65f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(flapButton)) 
+        {
+            rb2d.AddForce(transform.up * flapForce, ForceMode2D.Impulse);
+        }
+    }
+}
